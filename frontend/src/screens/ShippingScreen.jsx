e6a8c831033a5edcea7bcf61f82326fbf0ef2091
Peolahh <104804036+Peolahh@users.noreply.github.com>
2024-05-26 11:51:27 -0300

    version 1.1

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -8,14 +8,12 @@ import { saveShippingAddress } from '../slices/cartSlice';
 
 const ShippingScreen = () => {
   const cart = useSelector((state) => state.cart);
-  const { shippingAddress } = cart;
+  const { shippingAddress } = cart || {};
 
-  const [address, setAddress] = useState(shippingAddress.address || '');
-  const [city, setCity] = useState(shippingAddress.city || '');
-  const [postalCode, setPostalCode] = useState(
-    shippingAddress.postalCode || ''
-  );
-  const [country, setCountry] = useState(shippingAddress.country || '');
+  const [address, setAddress] = useState(shippingAddress?.address || '');
+  const [city, setCity] = useState(shippingAddress?.city || '');
+  const [postalCode, setPostalCode] = useState(shippingAddress?.postalCode || '');
+  const [country, setCountry] = useState(shippingAddress?.country || '');
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -84,3 +82,4 @@ const ShippingScreen = () => {
 };
 
 export default ShippingScreen;
+
